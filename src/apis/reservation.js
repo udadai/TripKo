a3abfe61e6261aff39dcc91f0c009783ad17351a
Dongjin <ehdwls1638@gmail.com>
2023-11-14 14:58:39 +0900

    fix : reservation request form

--- src/apis/reservation.js
+++ src/apis/reservation.js
@@ -1,30 +1,34 @@
 import instance from "./api";
 
 export const getReservation = async () => {
-  const restaurant = await instance.get("/userinfo/reservation/restaurant");
-  const festival = await instance.get("/userinfo/reservation/festival");
-  return {
-    restaurant: restaurant.data.response,
-    festival: festival.data.response,
-  };
+    const restaurant = await instance.get("/userinfo/reservations/restaurant");
+    const festival = await instance.get("/userinfo/reservations/festival");
+    return {
+        restaurant: restaurant.data.response,
+        festival: festival.data.response,
+    };
 };
 
 export const reserveFestival = async (id, date, headCount) => {
-  const response = await instance.post("/festival/bookings", {
-    placeId:id,
-    reservationDate:date,
-    headCount:headCount,
-  });
-  return response.data.response;
+    const response = await instance.post("/festival/bookings", {
+        reservation: {
+            placeId: id,
+            reservationDate: date,
+            headCount: headCount
+        }
+    });
+    return response.data.response;
 };
 
 export const reserveRestaurant = async (id, date, time, headCount, message) => {
-  const response = await instance.post("/restaurant/bookings", {
-    placeId: id,
-    reservationDate: date,
-    reservationTime: time,
-    headCount: headCount,
-    message: message,
-  });
-  return response.data.response;
+    const response = await instance.post("/restaurant/bookings", {
+        reservation: {
+            placeId: id,
+            reservationDate: date,
+            reservationTime: time,
+            headCount: headCount,
+            message: message,
+        }
+    });
+    return response.data.response;
 };

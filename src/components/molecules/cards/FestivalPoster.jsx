7dd0a65b588cca353cbc0d7c31123157c2c14d80
Dongjin <ehdwls1638@gmail.com>
2023-11-14 23:26:32 +0900

    fix : fix wishlist redirection

--- src/components/molecules/cards/FestivalPoster.jsx
+++ src/components/molecules/cards/FestivalPoster.jsx
@@ -7,19 +7,20 @@ const FestivalPoster = ({ image, to, alt, isWished, id }) => {
   const [wished, setWished] = useState(isWished);
   return (
     <div className="festival-card relative h-[26rem] w-[22rem] flex-shrink-0 overflow-hidden">
-      <Link to={to} className={"w-full h-full duration-300 hover:scale-105  transition-all"}>
-        <div className="absolute right-4 top-4">
-          <WishButton
-            filter={"touristSpot"}
-            id={id}
-            initialIsWished={wished}
-            onWishChange={(newWishState) => setWished(newWishState)}
-          />
-        </div>
+      <div className="absolute right-4 top-4">
+        <WishButton
+          filter={"touristSpot"}
+          id={id}
+          initialIsWished={wished}
+          onWishChange={(newWishState) => setWished(newWishState)}
+        />
+      </div>
+      <Link
+        to={to}
+        className={"h-full w-full"}
+      >
         <Photo
-          className={
-            "h-full w-full object-cover "
-          }
+          className={"h-full w-full object-cover "}
           src={image}
           alt={alt}
         />
--- src/components/molecules/cards/PlacePoster.jsx
+++ src/components/molecules/cards/PlacePoster.jsx
@@ -17,10 +17,7 @@ const PlacePoster = ({
 }) => {
   const [wished, setWished] = useState(isWished);
   return (
-    <Link
-      to={to ? to : "/"}
-      className="place-card shadow-rounded-card relative flex flex-shrink-0 flex-col justify-between gap-1 p-2 md:w-[15rem]"
-    >
+    <div className="place-card shadow-rounded-card relative flex flex-shrink-0 flex-col justify-between gap-1 p-2 md:w-[15rem]">
       <div className="absolute right-4 top-4">
         <WishButton
           filter={"touristSpot"}
@@ -29,32 +26,36 @@ const PlacePoster = ({
           onWishChange={(newWishState) => setWished(newWishState)}
         />
       </div>
-      <div className={"place-card-header"}>
-        <div
-          className={
-            "image-wrapper min-h-[11rem] w-full overflow-hidden rounded-lg md:h-[15rem]"
-          }
-        >
-          <Photo
-            src={image}
-            alt={alt}
-            className={"h-full w-full object-cover"}
-          />
+      <Link to={to ? to : "/"}>
+        <div className={"place-card-header"}>
+          <div
+            className={
+              "image-wrapper min-h-[11rem] w-full overflow-hidden rounded-lg md:h-[15rem]"
+            }
+          >
+            <Photo
+              src={image}
+              alt={alt}
+              className={"h-full w-full object-cover"}
+            />
+          </div>
+          <CardTitle title={name} lineClamp={2} />
         </div>
-        <CardTitle title={name} lineClamp={2} />
-      </div>
-      <div className={"place-card-body flex flex-col items-center text-sm"}>
-        <div className="place-card-address line-clamp-2 w-full">{address}</div>
-        <div
-          className={
-            "place-card-body flex w-full items-center justify-start text-sm"
-          }
-        >
-          <StarRating averageRating={averageRating} />
-          {averageRating.toFixed(1)}
+        <div className={"place-card-body flex flex-col items-center text-sm"}>
+          <div className="place-card-address line-clamp-2 w-full">
+            {address}
+          </div>
+          <div
+            className={
+              "place-card-body flex w-full items-center justify-start text-sm"
+            }
+          >
+            <StarRating averageRating={averageRating} />
+            {averageRating.toFixed(1)}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 

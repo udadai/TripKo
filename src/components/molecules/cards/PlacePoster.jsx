ace5f9eb95086eb0f0069266b627b50ebf2f2a90
udadai <yujeong470@gmail.com>
2023-11-14 23:11:34 +0900

    fix : wishbutton in place poster 페이지 이동 수정

--- src/components/molecules/cards/PlacePoster.jsx
+++ src/components/molecules/cards/PlacePoster.jsx
@@ -17,10 +17,39 @@ const PlacePoster = ({
 }) => {
   const [wished, setWished] = useState(isWished);
   return (
-    <Link
-      to={to ? to : "/"}
-      className="place-card shadow-rounded-card relative flex flex-shrink-0 flex-col justify-between gap-1 p-2 md:w-[15rem]"
-    >
+    <div className="place-card relative flex-shrink-0 overflow-hidden">
+      <Link
+        to={to ? to : "/"}
+        className="shadow-rounded-card relative flex flex-shrink-0 flex-col justify-between gap-1 p-2 md:w-[15rem]"
+      >
+        <div className={"place-card-header"}>
+          <div
+            className={
+              "image-wrapper min-h-[11rem] w-full overflow-hidden rounded-lg md:h-[15rem]"
+            }
+          >
+            <Photo
+              src={image}
+              alt={alt}
+              className={"h-full w-full object-cover"}
+            />
+          </div>
+          <CardTitle title={name} lineClamp={2} />
+        </div>
+        <div className={"place-card-body flex flex-col items-center text-sm"}>
+          <div className="place-card-address line-clamp-2 w-full">
+            {address}
+          </div>
+          <div
+            className={
+              "place-card-body flex w-full items-center justify-start text-sm"
+            }
+          >
+            <StarRating averageRating={averageRating} />
+            {averageRating.toFixed(1)}
+          </div>
+        </div>
+      </Link>
       <div className="absolute right-4 top-4">
         <WishButton
           filter={"touristSpot"}
@@ -29,32 +58,7 @@ const PlacePoster = ({
           onWishChange={(newWishState) => setWished(newWishState)}
         />
       </div>
-      <div className={"place-card-header"}>
-        <div
-          className={
-            "image-wrapper min-h-[11rem] w-full overflow-hidden rounded-lg md:h-[15rem]"
-          }
-        >
-          <Photo
-            src={image}
-            alt={alt}
-            className={"h-full w-full object-cover"}
-          />
-        </div>
-        <CardTitle title={name} lineClamp={2} />
-      </div>
-      <div className={"place-card-body flex flex-col items-center text-sm"}>
-        <div className="place-card-address line-clamp-2 w-full">{address}</div>
-        <div
-          className={
-            "place-card-body flex w-full items-center justify-start text-sm"
-          }
-        >
-          <StarRating averageRating={averageRating} />
-          {averageRating.toFixed(1)}
-        </div>
-      </div>
-    </Link>
+    </div>
   );
 };
 
